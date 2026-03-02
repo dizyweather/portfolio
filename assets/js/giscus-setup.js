2aabf4574077a154617123a6dd8ee735a5064d0e
dizyweather <geodzh@gmail.com>
2026-03-02 20:15:13 +0000

    Deploying to gh-pages from @ dizyweather/dizyweather.github.io@ec6734b6ab0a8195e89b755e1d81b03ccf18636b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dizyweather/portfolio",
+    "data-repo": "dizyweather/dizyweather.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
